c67b88d8cf7012968825ff043e0604b04f89916c
Kaspar Vollenweider <vok@panter.ch>
2020-11-05 14:07:44 +0100

    fix build arg passing. the docker documentation is so stupid. It does not work the way they documented in there main documentation.

--- scripts/build-all.sh
+++ scripts/build-all.sh
@@ -19,42 +19,41 @@ done
 # base image
 ./scripts/build-image.sh ${PUSH_AFTER_BUILD} \
   --tag='fermium' \
-  --build-arg=from_tag='fermium-slim'
+  --build-arg 'from_tag=fermium-slim'
 
 ./scripts/build-image.sh ${PUSH_AFTER_BUILD} \
   --df=chrome \
   --tag='fermium-chrome' \
   --suffix='86 latest' \
-  --build-arg=from_tag='fermium'
-
+  --build-arg 'from_tag=fermium'
 
 ./scripts/build-image.sh ${PUSH_AFTER_BUILD} \
   --df=cypress \
   --tag='fermium-cypress' \
   --suffix='latest 5.5.0' \
-  --build-arg=cypress_version='5.5.0' \
-  --build-arg=from_tag='fermium'
+  --build-arg 'cypress_version=5.5.0' \
+  --build-arg 'from_tag=fermium'
 
-./scripts/build-image.sh ${PUSH_AFTER_BUILD} \
-  --df=cypress \
-
-  --tag='fermium-chrome-cypress' \
-  --suffix='latest 5.5.0' \
-  --build-arg=cypress_version='5.5.0' \
-  --build-arg=from_tag='fermium-chrome'
+# ./scripts/build-image.sh ${PUSH_AFTER_BUILD} \
+#   --df=cypress \
+#   --tag='fermium-chrome-cypress' \
+#   --suffix='latest 5.5.0' \
+#   --build-arg 'cypress_version=5.5.0' \
+#   --build-arg 'from_tag=fermium-chrome'
 
 ## angular 10.1.1
 #
 ./scripts/build-image.sh ${PUSH_AFTER_BUILD} \
   --df=ng \
   --tag=fermium-ng-10.1.1  \
-  --build-arg=angular_cli_version='10.1.1' \
-  --build-arg=from_tag='fermium'
+  --build-arg 'angular_cli_version=10.1.1' \
+  --build-arg 'from_tag=fermium'
 
 ./scripts/build-image.sh ${PUSH_AFTER_BUILD} \
   --df=ng \
   --tag='fermium-chrome-ng-10.1.1' \
-  --build-arg=from_tag='fermium-chrome'
+  --build-arg 'angular_cli_version=10.1.1' \
+  --build-arg 'from_tag=fermium-chrome'
 
 ## angular 10.2.2
 #
@@ -62,22 +61,22 @@ done
   --df=ng \
   --tag=fermium-ng \
   --suffix='10.2.2 latest' \
-  --build-arg=angular_cli_version='10.2.2' \
-  --build-arg=from_tag='fermium'
+  --build-arg 'angular_cli_version=10.2.2' \
+  --build-arg 'from_tag=fermium'
 
 ./scripts/build-image.sh ${PUSH_AFTER_BUILD} \
   --df=ng \
   --tag=fermium-chrome-ng \
   --suffix='10.2.2 latest' \
-  --build-arg=angular_cli_version='10.2.2' \
-  --build-arg=from_tag='fermium-chrome'
+  --build-arg 'angular_cli_version=10.2.2' \
+  --build-arg 'from_tag=fermium-chrome'
 
 ./scripts/build-image.sh ${PUSH_AFTER_BUILD} \
   --df=ng \
   --tag=fermium-cypress-ng \
   --suffix='10.2.2 latest' \
-  --build-arg=angular_cli_version='10.2.2' \
-  --build-arg=from_tag='fermium-cypress'
+  --build-arg 'angular_cli_version=10.2.2' \
+  --build-arg 'from_tag=fermium-cypress'
 
 echo $'# node-extra-packages\n' > README.md
 echo $'## Docker tags availables\n' >> README.md
